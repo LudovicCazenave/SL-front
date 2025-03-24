d4c1b0a0e695cd681c072d0242dde013bcc7c7db
LudovicCazenave <lcazenav@gmail.com>
2025-03-24 11:43:30 +0100

    header logout done

---
+++ src/assets/SIgnUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import Container  from 'react-bootstrap/Container'
+import SignUpImage from '../SignUpImage/SignUpImage.jsx'
+function SignUp (){
+  return (
+    <>
+      <Container fluid="xl" className="bg-white p-3 position-relative" >
+        <SignUpImage />
+      </Container>
+    </>
+  )
+}
+
+export default SignUp
