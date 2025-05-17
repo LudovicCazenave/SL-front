bd152a0b2b672e397aadae144c9ce14479525221
LudovicCazenave <lcazenav@gmail.com>
2025-05-17 17:37:59 +0200

    wip

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import { authentificationUser } from "../api/api.js";
 
 // Create the authentication context to share the auth state across the app
