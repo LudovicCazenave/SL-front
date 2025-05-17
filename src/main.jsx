9e3b0fb9c1184cca3f9fd5e997cb74abf67271f6
LudovicCazenave <lcazenav@gmail.com>
2025-05-17 17:50:06 +0200

    bug classes bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import App from './App/App.jsx'
 
